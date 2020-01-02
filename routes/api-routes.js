8c976a72bb34b52b913d30b9e00bd1a3c64d94ff
Mary Frances <mfskibiel@gmail.com>
2020-01-02 11:15:57 -0500

    api routes for authorization and added middleware file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,59 @@
 // const db = require("../models");
 const path = require("path");
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const authWare = require("../middleware/authware");
+
 // const cloud = require("../nodejs/cloudinaryUp");
 
-
 module.exports = function(app) {
+  app.post("/api/signup", function(req, res) {
+    User.create(req.body)
+      .then(function(result) {
+        res.json({ message: "user created" });
+      })
+      .catch(function(err) {
+        res.status(500).json({ error: err.message });
+      });
+  });
 
-}
+  app.post("/api/authenticate", function(req, res) {
+    const { username, password } = req.body;
+    User.findOne({ username: username }).then(function(dbUser) {
+      if (!dbUser)
+        return res
+          .status(401)
+          .json({ message: "username or password is incorrect" });
+      if (dbUser.comparePassword(password)) {
+        const token = jwt.sign(
+          {
+            data: dbUser._id
+          },
+          "superSecretKey"
+        );
+
+        res.json({
+          id: dbUser._id,
+          username: dbUser.username,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: "username or password incorrect" });
+      }
+    });
+  });
+
+  app.get("/api/protected", authWare, function(req, res) {
+    const user = req.user;
+    res.json({
+      message:
+        user.username + ", you should only see this if you're authenticated."
+    });
+  });
+
+  app.get("/api/public", function(req, res) {
+    res.json({
+      message: "this is available for everyone"
+    });
+  });
+};
