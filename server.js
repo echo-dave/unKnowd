b9181560967d5736215cef7bf6f4c395de6d5c69
Dave May <echo.dave@protonmail.com>
2020-01-08 10:04:50 -0500

    partially working file uploads. Locally not remootely

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const path = require("path");
 const app = express();
 var http = require("http").createServer(app);
 var io = require("socket.io")(http);
+const fileUpload = require("express-fileupload");
 
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
+app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
