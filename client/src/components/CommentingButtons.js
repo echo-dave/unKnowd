39821f00475c6cff051e0ff6d30a1f88398cbeb6
Dave May <echo.dave@protonmail.com>
2020-01-13 19:33:37 -0500

    comment and reply buttons are now stateful and auth'd against read only viewer

--- client/src/components/CommentingButtons.js
+++ client/src/components/CommentingButtons.js
@@ -1,24 +1,39 @@
 import React, { Component } from "react";
-
+import Auth from "../utils/Auth";
 class CommentingButtons extends Component {
+  state = {
+    loggedIn: false
+  };
+
+  componentDidMount() {
+    this.setState({
+      loggedIn: Auth.isLoggedIn()
+    });
+    setTimeout(() => console.log("login State", this.state.loggedIn), 500);
+  }
+
   render() {
     return (
-      <div className="commentingButtons">
-        <button
-          className="button is-smaller"
-          style={{ marginRight: ".5rem" }}
-          onClick={this.props.toggleReply}
-        >
-          Reply
-        </button>
-        <button
-          className="button is-smaller"
-          data-id={this.props.dataId}
-          onClick={this.props.toggleComments}
-        >
-          Comments
-        </button>
-      </div>
+      <>
+        {this.state.loggedIn ? (
+          <div className="commentingButtons">
+            <button
+              className="button is-smaller"
+              style={{ marginRight: ".5rem" }}
+              onClick={this.props.toggleReply}
+            >
+              Reply
+            </button>
+            <button
+              className="button is-smaller"
+              data-id={this.props.dataId}
+              onClick={this.props.toggleComments}
+            >
+              Comments
+            </button>
+          </div>
+        ) : null}
+      </>
     );
   }
 }
