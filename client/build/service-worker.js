3ca2d77a6f63d21a69fb250d856e80f719a52482
Echo <echo.dave@protonmail.com>
2020-02-02 02:45:35 -0500

    pm2 script correction

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c22b9cede3d7d3d278e9b44ca4136844.js"
+  "/precache-manifest.dbc1bc60b15c7aa994165bb27fc0f9f7.js"
 );
 
 self.addEventListener('message', (event) => {
