f1b896f0f26e0e78963af7e7b97f1262e3fd5b18
Echo <echo.dave@protonmail.com>
2020-02-02 02:17:34 -0500

    setting up for pm2 running and monitoring

---
+++ client/build/precache-manifest.dbc1bc60b15c7aa994165bb27fc0f9f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a0f1c6cea85053aff086ff7529dbff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75646fcc86d744446db8",
+    "url": "/static/css/2.b90ce945.chunk.css"
+  },
+  {
+    "revision": "76662afeeca01e7a510d",
+    "url": "/static/css/main.47ab91a4.chunk.css"
+  },
+  {
+    "revision": "75646fcc86d744446db8",
+    "url": "/static/js/2.36773128.chunk.js"
+  },
+  {
+    "revision": "53286e59b5a78920069b15ca83bfde84",
+    "url": "/static/js/2.36773128.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76662afeeca01e7a510d",
+    "url": "/static/js/main.c51a59e0.chunk.js"
+  },
+  {
+    "revision": "f7c487cf8d5f7e42c0bf",
+    "url": "/static/js/runtime-main.0953d9ee.js"
+  }
+]);
