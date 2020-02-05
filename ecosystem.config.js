1f257d46212586c66c420f9b7bc88d62e9130ae8
Echo <echo.dave@protonmail.com>
2020-02-05 06:06:31 -0500

    disabling pm2 watch production

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       env_production: {
         NODE_ENV: "production",
+        watch:false,
         ignore_watch: ["client/public/upload","node_modules"],
         exec_mode: "cluster",
         instances: "max"
