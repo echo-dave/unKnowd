646800d0592e4da27f102732d2ca48bf69f4a100
Dave May <echo.dave@protonmail.com>
2021-07-16 00:14:56 -0400

    add path as static root

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ require("./routes/api-routes.js")(app, io);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use("/", express.static(path.join(__dirname, "client/build")));
 }
 
 // Send every other request to the React app
